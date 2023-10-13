073d421c5b818304f104c91b83bbc901f0aebdb3
Wahyu Kusuma Wardhana <wahyukusumaw29@gmail.com>
2023-10-13 09:56:56 +0700

    Add comment sortCarByYearAscendingly.js

--- scripts/sortCarByYearAscendingly.js
+++ scripts/sortCarByYearAscendingly.js
@@ -3,8 +3,12 @@ function sortCarByYearAscendingly(cars) {
   const result = [...cars];
 
   // Tulis code-mu disini
+
+  // Loop mulai dari index 0
   for (let i = 0; i < result.length; i++) {
+    //Loop mulai dari index 1
     for (let j = 0; j < result.length - 1; j++) {
+      // Algoritma Bubble Sort
       if (result[j].year > result[j + 1].year) {
         let temp = result[j];
         result[j] = result[j + 1];
